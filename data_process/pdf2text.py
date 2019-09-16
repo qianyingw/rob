bb9fa2a08b08fd15565d4e1169043a12dbd7bf81
Qianying Wang <w.qianying@outlook.com>
2019-09-16 14:49:58 +0100

    Update pdf2text.py
    Add function for processing encrypted pdf

--- data_process/pdf2text.py
+++ data_process/pdf2text.py
@@ -8,8 +8,8 @@ convert_pdf_to_txt(path_to_file)
     path_to_file is pdf's full path
 
 convert_multiple(path_df)
-    Input dataframe: path_df [Id, path_df] (full paths of pdfs)
-    Output dataframe: text_df [Id, text] 
+    Input dataframe: path_df [ID, path_df] (full paths of pdfs)
+    Output dataframe: text_df [ID, text] 
 """
 
 import os
@@ -23,57 +23,101 @@ from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
 
+import pikepdf
+import PyPDF2
+
+
 #%% Convert single pdf
 def convert_pdf_to_txt(path_to_file):
-    if not os.path.exists(path_to_file):
-        text = ''
-        print('No such file: %s' % path_to_file)
-    else:       
-        rsrcmgr = PDFResourceManager()
-        retstr = StringIO()
-        codec = 'utf-8'
-        laparams = LAParams()
-        device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
-        fp = open(path_to_file, 'rb')
-        interpreter = PDFPageInterpreter(rsrcmgr, device)
-        password = ""
-        maxpages = 0
-        caching = True
-        pagenos=set()
-                 
-        try:
+    
+    # Read txt file directly
+    if path_to_file.lower().endswith('.txt'):
+        with open(path_to_file, 'r') as fp:
+            text = fp.read()
+
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos=set()
+    
+    if os.path.exists(path_to_file):
+        fp = open(path_to_file, 'rb')   
+        reader = PyPDF2.PdfFileReader(fp)
+        if reader.isEncrypted == True:
+            pdf = pikepdf.open(path_to_file)
+            new_path = path_to_file.split('.pdf')[0]+'_extractable.pdf'
+            pdf.save(new_path)
+            fp = open(new_path, 'rb')  
+                                  
+        try:   
             for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password, caching=caching, check_extractable=True):
-                interpreter.process_page(page)
-            text = retstr.getvalue()
-            fp.close()
-            device.close()
-            retstr.close()      
+                interpreter.process_page(page)  
+            text = retstr.getvalue()                              
         except:
             text = ''
-            print("Cannot convert: " + os.path.basename(path_to_file))
+            print('Cannot be converted: {}.'.format(path_to_file))
+        fp.close()
+        device.close()
+        retstr.close()  
+            
+    else:  
+        text = ''
+        print('No such file: %s' % path_to_file)
     
     return text
 
 
 #%% Converts multiple pdfs
-# Input dataframe: path_df: [Id, path_df] (full path of pdfs)
-# Output dataframe: text_df: [Id, text] 
-def convert_multiple(path_df):
+# Input dataframe: path_df: [ID, path_df] (full path of pdfs)
+# Output dataframe: text_df: [ID, text] 
+def convert_multiple(path_df, out_path):
     cnum = 0
     pdfnum = 0
-    text_df = pd.DataFrame(columns=['Id', 'Text'])
+    pdf_fail = []
+    text_df = pd.DataFrame(columns=['ID', 'Text'])
     
-    for i in range(len(path_df)):
-        pdf_path = path_df.loc[i,'pdf_path']
+    for i, row in path_df.iterrows():
+        pdf_path = row['pdf_path']
         if (not os.path.isdir(pdf_path)) and (os.path.splitext(pdf_path)[-1].lower() =='.pdf'):
             pdfnum = pdfnum + 1   
+            
             text = convert_pdf_to_txt(pdf_path)
             text = re.sub(r'\s+', ' ', text)
             
-            text_df = text_df.append({'Id': path_df.loc[i,'Id'], 'Text': text}, ignore_index=True)
+            out_filename = out_path + 'np_{}.txt'.format(row['ID'])
+            with open(out_filename, 'w', encoding="utf-8") as fout:
+                fout.write(text)                
+#            print('Record {} is finished.'.format(i))
+            
+            text_df = text_df.append({'ID': row['ID'], 'Text': text}, ignore_index=True)
             if text != '':
                 cnum = cnum + 1
+            else:
+                pdf_fail.append(row['ID'])
+    
+#    for i in range(len(path_df)):
+#        pdf_path = path_df.loc[i,'pdf_path']
+#        if (not os.path.isdir(pdf_path)) and (os.path.splitext(pdf_path)[-1].lower() =='.pdf'):
+#            pdfnum = pdfnum + 1   
+#            text = convert_pdf_to_txt(pdf_path)
+#            text = re.sub(r'\s+', ' ', text)
+#            print('Record {} is finished.'.format(i))
+#            text_df = text_df.append({'ID': path_df.loc[i,'ID'], 'Text': text}, ignore_index=True)
+#            if text != '':
+#                cnum = cnum + 1
+#            else:
+#                pdf_fail.append()
     
     print('\n{0} pdfs in total.'.format(pdfnum))            
     print('{0} pdfs has been converted to texts.'.format(cnum))
+    if len(pdf_fail) != 0:
+        print('Failed IDs:\n')
+        print(pdf_fail)
+    
     return text_df
